1d8d199c9051b7379a8ad7d30f3743835414e1e9
oldtomcat <oldtomcat@example.com>
2026-09-01 09:50:20 +0800

    v1.0.0-beta5: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.oldtomcat.taxgroup"
         minSdk = 24
         targetSdk = 36
-        versionCode = 103
-        versionName = "1.0.0-beta4"
+        versionCode = 104
+        versionName = "1.0.0-beta5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
